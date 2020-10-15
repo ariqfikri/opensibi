261c451e4fafec638593eb4d668dffdde370da5e
ariqfikri <lolicodesu@gmail.com>
2020-10-15 08:28:16 +0700

    first commit

---
+++ myo/views.py
@@ -0,0 +1,36 @@
+from django.http import JsonResponse
+from django.shortcuts import render
+from django.http import HttpResponse
+from django.http import HttpRequest
+from rest_framework.decorators import api_view
+from sklearn.neighbors import KNeighborsClassifier
+from rest_framework.response import Response
+from rest_framework import serializers
+from django.db import connection
+import numpy as np
+
+
+class MyoSerializer(serializers.Serializer):
+    hasil = serializers.CharField(max_length=200)
+    
+
+class Myo(object):
+    def __init__(self, hasil):
+        self.hasil = hasil
+        
+
+@api_view()
+def myo(request):
+  with connection.cursor() as cursor:
+    cursor.execute("SELECT * FROM newlabel")
+    train_label = cursor.fetchall()
+    cursor.execute("SELECT * FROM newdata")
+    train_data = cursor.fetchall()
+
+  knn = KNeighborsClassifier(n_neighbors = 3, weights = 'distance')
+  knn.fit(train_data,train_label)
+  predict = knn.predict(train_data)
+  kirim = Myo(hasil = predict)
+  serializer = MyoSerializer(kirim)
+  return Response(serializer.data)
+# Create your views here.
